09b149ab1733610125c80f81909ba145c459a2db
Katerina Korre <73671180+katkorre@users.noreply.github.com>
2023-01-05 20:38:58 +0100

    Update sent_similarity.py

--- sent_similarity.py
+++ sent_similarity.py
@@ -11,7 +11,7 @@ from ast import literal_eval
 from zipfile import *
  
  
-df = pd.read_csv("/home/akorre/hate_laws/hate_laws.csv)
+df = pd.read_csv("/home/akorre/hate_laws/hate_laws.csv")
 
 # which countries do not have laws on hate speech
 nolaw = df['Country'].loc[df['Legislation'].str.startswith('–')].to_list()
